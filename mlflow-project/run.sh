b8aff2eab03000b1e8b1e7452edc16d6b01f131a
Xavier Ngansop <ngaxavi@gmail.com>
2020-08-12 18:10:52 +0200

    fix: environment variable and docker compose file

--- mlflow-project/run.sh
+++ mlflow-project/run.sh
@@ -18,8 +18,14 @@ success() {
 explain "Build environment"
 tell docker build -t mlflow-docker-env -f Dockerfile .
 
-explain "Set Tracking endpoint"
-tell export MLFLOW_TRACKING_URI=http://127.0.0.1:5000
+#explain "Set Tracking endpoint"
+tell export MLFLOW_TRACKING_URI=http://192.168.1.40:5000 # set your ip addr mlfow-server
+tell export MLFLOW_EXPERIMENT_ID=1
+#
+#explain "Set Credentials for access to buckets"
+tell export MLFLOW_S3_ENDPOINT_URL=http://192.168.1.40:9000 # set your ip addr minio
+tell export AWS_ACCESS_KEY_ID=secret_mlflow
+tell export AWS_SECRET_ACCESS_KEY=secret_mlflow
 
 explain "Run many experiments"
 tell mlflow run . -P alpha=0.1
